b371ee335ba8787a18a787116ca0c3e0990ddc7a
Adam <awarde@tcd.ie>
2017-11-17 04:40:14 -0800

    Added some features for logged in dev

--- githubAPI.py
+++ githubAPI.py
@@ -8,5 +8,14 @@ g = Github(username,pw)
 
 for repo in g.get_user().get_repos():
     print repo.name
+    #for commit in repo.get_commits():
+        #print commit
+    for collaborators in repo.get_collaborators():
+        print collaborators
+    for languages in repo.get_languages():
+       print languages
+
+email = raw_input("Email:")
+print g.legacy_search_user_by_email(email)
 
 print g.get_last_api_status_message()
