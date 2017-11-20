8e3764f2434e990cea23907c43e7451763c6b72c
Adam <awarde@tcd.ie>
2017-11-20 08:00:31 -0800

    Added a few features

--- githubAPI.py
+++ githubAPI.py
@@ -8,12 +8,20 @@ g = Github(username,pw)
 
 for repo in g.get_user().get_repos():
     print repo.name
-    #for commit in repo.get_commits():
+    numberOfCommits = 0
+    for commit in repo.get_commits():
         #print commit
+        numberOfCommits += 1
+    print "Number of Commits: " + repr(numberOfCommits)
     for collaborators in repo.get_collaborators():
         print collaborators
     for languages in repo.get_languages():
        print languages
+    for branch in repo.get_branches():
+        print branch
+        print ""
+    print repo.get_stats_contributors()
+
 
 email = raw_input("Email:")
 print g.legacy_search_user_by_email(email)
